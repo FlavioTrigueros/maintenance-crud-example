c0a4476ee1b8c9b85d7fb36b29e574c922427e20
Flavio Eduardo Trigueros Chumacero <U202210190@upc.edu.pe>
2025-12-28 16:34:33 -0500

    feat: update angular webpacks

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
-console.log('[main] bootstrapping AppModule ...');
-
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
-  .then(() => console.log('[main] bootstrap OK'))
-  .catch(err => {
-    console.error('[main] bootstrap FAILED - full error below:');
-    console.error(err);
-    throw err;
-  });
+  .catch(err => console.error(err));
